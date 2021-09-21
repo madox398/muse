773a7735574e592e12224547479b2a90e06737e6
madox398 <41486858+madox398@users.noreply.github.com>
2021-09-21 19:44:42 +0200

    Change message to polish language

--- src/commands/clear.ts
+++ src/commands/clear.ts
@@ -9,7 +9,7 @@ export default class implements Command {
   public name = 'clear';
   public aliases = ['c'];
   public examples = [
-    ['clear', 'clears all songs in queue except currently playing']
+    ['clear', 'usuwa wszystkie utwory z kolejki oprócz aktualnie odtwarzanego']
   ];
 
   public requiresVC = true;
@@ -23,6 +23,6 @@ export default class implements Command {
   public async execute(msg: Message, _: string []): Promise<void> {
     this.playerManager.get(msg.guild!.id).clear();
 
-    await msg.channel.send('clearer than a field after a fresh harvest');
+    await msg.channel.send('już wyczyszczone szefie');
   }
 }
